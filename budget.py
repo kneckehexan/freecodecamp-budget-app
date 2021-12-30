538727abf26470b0549ee5f87f7fe30e71ee7add
Philip Tunbjer <philip.tunbjer@gmail.com>
2021-12-30 23:27:08 +0100

    minor code formatting

--- budget.py
+++ budget.py
@@ -7,7 +7,7 @@ class Category:
     def deposit(self, amount, descr=''):
         """Add a positive transaction to the ledger."""
         self.ledger.append({"amount": amount, "description": descr})
-    
+
     def withdraw(self, amount, descr=''):
         """Add a negative transaction to the ledger (if possible)."""
         if self.check_funds(amount):
@@ -18,11 +18,12 @@ class Category:
     def get_balance(self):
         """Get the current balance in the ledger"""
         return sum(item["amount"] for item in self.ledger)
-    
+
     def transfer(self, amount, category):
         """Transfer an amount to another ledger (if possible)."""
         if self.check_funds(amount):
-            self.ledger.append({"amount": -amount, "description": "Transfer to " + category.name})
+            self.ledger.append(
+                {"amount": -amount, "description": "Transfer to " + category.name})
             category.deposit(amount, "Transfer from " + self.name)
             return True
         return False
@@ -33,7 +34,8 @@ class Category:
 
     def __str__(self):
         halfNameLen = round(len(self.name)/2)
-        display = '*' * (15 - halfNameLen) + self.name + '*' * (15 - halfNameLen) + '\n'
+        display = '*' * (15 - halfNameLen) + self.name + \
+            '*' * (15 - halfNameLen) + '\n'
         for entry in self.ledger:
             descOrig = entry['description']
             descNew = descOrig[:23] if len(descOrig) > 23 else descOrig
@@ -42,11 +44,9 @@ class Category:
             amount = "%0.2f" % (entry['amount'])
             display += ' ' * (7 - len(amount))
             display += amount + '\n'
-        
 
         display += 'Total: ' + str(self.get_balance())
         return display
-        
 
 
 def create_spend_chart(categories):
@@ -55,9 +55,10 @@ def create_spend_chart(categories):
     # Store category name and the total of all negative values for each category in new data structure.
     catSum = []
     for cat in categories:
-        tmpsum = sum(item['amount'] for item in cat.ledger if item['amount'] < 0)
+        tmpsum = sum(item['amount']
+                     for item in cat.ledger if item['amount'] < 0)
         catSum.append({"category": cat.name, "sum": tmpsum})
-    
+
     # Calculate the total of all negative values.
     totalsum = sum(item["sum"] for item in catSum)
 
@@ -66,13 +67,13 @@ def create_spend_chart(categories):
     for i in reversed(range(11)):
         display += ' ' * (3 - len(str(i*10))) + str(i*10) + '|'
         for cs in catSum:
-            frac = cs["sum"]*10//totalsum*10 # Floor to 10th percent
+            frac = cs["sum"]*10//totalsum*10  # Floor to 10th percent
             if frac >= i*10:
                 display += ' o '
             else:
                 display += '   '
         display += ' \n'
-    display += ' ' * 4 + '-' * len(catSum) * 3 + '-\n' # 'x'-axis
+    display += ' ' * 4 + '-' * len(catSum) * 3 + '-\n'  # 'x'-axis
 
     # Creating the labels for the 'x'-axis
     display += ' ' * 5
